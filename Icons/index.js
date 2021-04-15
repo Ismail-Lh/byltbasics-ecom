34e066d0d98bf466c55adfd6bbe856133d48bcbc
Ismail-Lh <ismailsaad03@gmail.com>
2021-04-15 16:11:25 +0000

    Add scss variables & svg icons

---
+++ Icons/index.js
@@ -0,0 +1,4 @@
+export { default as CartIcon } from './CartIcon';
+export { default as ContactIcon } from './ContactIcon';
+export { default as LoginIcon } from './LoginIcon';
+export { default as SearchIcon } from './SearchIcon';
