d0e830d82aae2a1f6d7841d8dce025726f55ce6e
Ismail-Lh <ismailsaad03@gmail.com>
2021-04-23 15:52:00 +0000

    Add the responsive design to the SplitContentSection part-1

--- sections/SplitContentSection/SplitContentSectionStyles.jsx
+++ sections/SplitContentSection/SplitContentSectionStyles.jsx
@@ -4,6 +4,27 @@ export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: ${props =>
     props.isTextFirst ? '40% 60%' : '60% 40%'};
-
   height: 54rem;
+
+  @media (max-width: 1024px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 60rem 50rem;
+    height: auto;
+
+    div:first-of-type {
+      order: 2;
+    }
+  }
+
+  @media (max-width: 768px) {
+    grid-template-rows: repeat(2, 50rem);
+  }
+
+  @media (max-width: 425px) {
+    grid-template-rows: 25rem 50rem;
+  }
+
+  @media (max-width: 320px) {
+    grid-template-rows: 20rem 50rem;
+  }
 `;
