8ab3c171085f11d92d9ad5d0720aa591d81ff2af
Ismail-Lh <ismailsaad03@gmail.com>
2021-05-19 19:59:41 +0100

    Add the responsive design to the SingleProductImages compo

--- components/SingleProductImages/SingleProductImages.jsx
+++ components/SingleProductImages/SingleProductImages.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 import classes from './SingleProductImages.module.scss';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 
 const SingleProductImages = ({
   images,
@@ -8,25 +11,46 @@ const SingleProductImages = ({
   type,
   gender,
 }) => {
+  const { width } = useWindowDimensions();
+
+  const [imageIndex, setImageIndex] = useState(0);
+
   return (
-    <div className={classes.singleProduct_images}>
-      {images?.map((img, idx) => (
-        <div key={idx}>
-          <img
-            src={`/assets/products/${gender}/${category}/${type}/${name}/${color}/large/${img}`}
-            alt={`${name}-${color}`}
-          />
+    <>
+      {width <= 768 ? (
+        <div className={classes.singleProductImages_mobile}>
+          <div className={classes.big_image}>
+            <img
+              src={`/assets/products/${gender}/${category}/${type}/${name}/${color}/large/${images[imageIndex]}`}
+              alt={`${name}-${color}-${imageIndex}`}
+            />
+          </div>
+
+          <div className={classes.small_images}>
+            {images?.map((img, idx) => (
+              <div key={idx} onClick={() => setImageIndex(idx)}>
+                <img
+                  src={`/assets/products/${gender}/${category}/${type}/${name}/${color}/large/${img}`}
+                  alt={`${name}-${color}`}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
-    </div>
+      ) : (
+        <div className={classes.singleProductImages_desktop}>
+          {images?.map((img, idx) => (
+            <div key={idx}>
+              <img
+                src={`/assets/products/${gender}/${category}/${type}/${name}/${color}/large/${img}`}
+                alt={`${name}-${color}`}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
 export default SingleProductImages;
-
-{
-  /* <img
-        src={`assets/products/men/${category}/${type}/${name}/${color}/large/${images[0]}`}
-        alt={`${name}-${color}`}
-      /> */
-}
