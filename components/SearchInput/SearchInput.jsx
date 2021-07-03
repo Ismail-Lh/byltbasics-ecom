3a041633c525602419dc941ba7ee689684b704f7
Ismail-Lh <ismailsaad03@gmail.com>
2021-07-03 16:43:14 +0100

    Add the search functionality part-2

--- components/SearchInput/SearchInput.jsx
+++ components/SearchInput/SearchInput.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useProductsContext } from '../../contexts/products_context';
 import { CloseIcon } from '../../Icons';
 import MyLink from '../MyLink/MyLink';
@@ -9,27 +10,59 @@ const SearchInput = ({ closeSearchBar }) => {
 
   const allProducts = [...men_products, ...women_products];
 
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const handelSearch = e => {
+    const searchWord = e.target.value;
+    setSearchValue(searchWord);
+
+    const newFilter = allProducts.filter(({ name }) =>
+      name.toLowerCase().includes(searchWord.toLowerCase())
+    );
+
+    if (searchWord === '') setFilteredProducts([]);
+    else setFilteredProducts(newFilter);
+  };
+
+  const handelClick = (id, gender, color) => {
+    setFilteredProducts([]);
+    setSearchValue('');
+    getSingleProduct(id, gender, color);
+  };
+
   return (
     <div className={classes.search}>
       <div className={classes.search__input}>
-        <input type='text' placeholder='search' />
+        <input
+          type='text'
+          placeholder='search'
+          value={searchValue}
+          onChange={handelSearch}
+        />
 
         <button onClick={() => closeSearchBar(false)}>
           <CloseIcon />
         </button>
       </div>
 
-      <div className={classes.search__results}>
-        {allProducts.map(({ name, id, route, gender, colors }) => (
-          <div onClick={() => getSingleProduct(id, gender, colors[0])}>
-            <MyLink
-              key={id}
-              route={`/products/${route}/?gender=${gender}&id=${id}`}>
-              {name}
-            </MyLink>
-          </div>
-        ))}
-      </div>
+      {filteredProducts.length !== 0 && (
+        <div className={classes.search__results}>
+          {filteredProducts
+            .slice(0, 10)
+            .map(({ name, id, route, gender, colors }) => (
+              <div
+                onClick={() => handelClick(id, gender, colors[0])}
+                className={classes.result}>
+                <MyLink
+                  key={id}
+                  route={`/products/${route}/?gender=${gender}&id=${id}`}>
+                  {name}
+                </MyLink>
+              </div>
+            ))}
+        </div>
+      )}
     </div>
   );
 };
