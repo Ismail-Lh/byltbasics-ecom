0ea5f605c88df7f453eef5b38855d81be34bd5b6
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-17 13:57:09 +0100

    feat(packages/types): implement shared types package
    This commit introduces a new package for shared type definitions across the project.
    The package is designed to provide a consistent and reusable set of types for API responses,
    enhancing type safety and maintainability.
    
    The following changes have been made:
    
    - Created a new package named `@byltbasics/types` to house shared type definitions.
    - Added interfaces for API responses, including success and error responses.
    - Configured the build system using `tsup` to compile TypeScript files into JavaScript.
    
    - Updated the root `package.json` to include a build script for the new types package.
    - Updated `.gitignore` to exclude the `dist` directories from version control.
    - Updated the `pnpm-lock.yaml` file to reflect the new package and its dependencies.

---
+++ packages/types/src/api/success-response.ts
@@ -0,0 +1,20 @@
+/**
+ * Represents a standardized structure for a successful API response.
+ *
+ * @template T - The type of the data included in the response payload.
+ *
+ * @property success - Indicates whether the API request was successful.
+ * @property statusCode - The HTTP status code associated with the response.
+ * @property payload - The main content of the response.
+ * @property payload.message - A descriptive message about the response.
+ * @property payload.data - (Optional) The data returned by the API, if any.
+ * @property payload.paginationMeta - (Optional) Metadata for paginated responses, if applicable.
+ */
+export interface IApiSuccessResponse<T> {
+  success: boolean;
+  statusCode: number;
+  payload: {
+    message: string;
+    data: T;
+  };
+}
