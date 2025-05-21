f2d3f8d9890117809b6c1c6566b445fe7113c59f
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-21 16:55:15 +0100

    feat(api/auth): add AuthLoginController to handle user login requests

---
+++ apps/express-app/src/presentation/http/controllers/auth/auth-login.controller.ts
@@ -0,0 +1,49 @@
+import { inject, injectable } from "inversify";
+
+import type { IApiResponseSanitizer } from "@/application/providers";
+import type { IAuthLoginUseCase } from "@/application/use-cases/auth";
+import type { IAuthLoginDto, IAuthResponseDto } from "@/domain/auth/dtos";
+
+import { TYPES } from "@/infrastructure/di-container/types";
+
+import type { IHttpRequest, IHttpResponse } from "../../helpers/interfaces";
+import type { IHttpController } from "../controller.interface";
+
+export interface IAuthLoginController extends IHttpController<IAuthLoginDto, IAuthResponseDto> {}
+
+/**
+ * Controller responsible for handling HTTP requests related to user login.
+ *
+ * @remarks
+ * This controller receives user login requests, delegates the business logic to the injected
+ * `AuthLoginUseCase`, and returns the appropriate HTTP response.
+ *
+ * @see IAuthLoginController
+ * @see IAuthLoginUseCase
+ */
+@injectable()
+export class AuthLoginController implements IAuthLoginController {
+  /**
+   * Creates an instance of the controller with the injected AuthLoginUseCase dependency.
+   *
+   * @param authLoginUseCase - The use case responsible for handling user login logic.
+   */
+  constructor(@inject(TYPES.AuthLoginUseCase) private authLoginUseCase: IAuthLoginUseCase, @inject(TYPES.ApiResponseSanitizer) private apiResponseSanitizer: IApiResponseSanitizer) {}
+
+  /**
+   * Handles the HTTP request to log in a user.
+   *
+   * @param request - The HTTP request containing the user login data in the body.
+   * @returns A promise that resolves to an HTTP response with the authentication tokens.
+   */
+  async handle(request: IHttpRequest<IAuthLoginDto>): Promise<IHttpResponse<IAuthResponseDto>> {
+    const body = request.body;
+
+    const authTokens = await this.authLoginUseCase.execute(body);
+
+    // Sanitize the response data
+    const sanitizedResponse = this.apiResponseSanitizer.successResponse({ data: authTokens, message: "User Logged In Successfully.", statusCode: 200 });
+
+    return sanitizedResponse;
+  }
+}
--- apps/express-app/src/presentation/http/controllers/auth/index.ts
+++ apps/express-app/src/presentation/http/controllers/auth/index.ts
@@ -1,1 +1,2 @@
+export * from "./auth-login.controller";
 export * from "./auth-register.controller";
