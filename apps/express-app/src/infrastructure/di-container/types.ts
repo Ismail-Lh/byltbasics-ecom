24060c41bd5ad1e118b29a15e8e23e018f09da39
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-21 16:58:03 +0100

    feat(api/di-container): add AuthLoginUseCase and AuthLoginController to types

--- apps/express-app/src/infrastructure/di-container/types.ts
+++ apps/express-app/src/infrastructure/di-container/types.ts
@@ -2,7 +2,10 @@ export const TYPES = {
   Logger: Symbol.for("Logger"),
   CryptoProvider: Symbol.for("CryptoProvider"),
   ApiResponseSanitizer: Symbol.for("ApiResponseSanitizer"),
+  JwtTokenProvider: Symbol.for("JwtTokenProvider"),
   UserRepository: Symbol.for("UserRepository"),
   CreateUserUseCase: Symbol.for("CreateUserUseCase"),
   AuthRegisterController: Symbol.for("AuthRegisterController"),
+  AuthLoginUseCase: Symbol.for("AuthLoginUseCase"),
+  AuthLoginController: Symbol.for("AuthLoginController"),
 } as const;
