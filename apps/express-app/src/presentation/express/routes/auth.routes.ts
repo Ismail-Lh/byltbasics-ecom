bd63966d90ff4aa87b558963f04dd7ebc354ee78
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-21 17:04:04 +0100

    feat(api/auth): implement login endpoint with cookie handling for access and refresh tokens

--- apps/express-app/src/presentation/express/routes/auth.routes.ts
+++ apps/express-app/src/presentation/express/routes/auth.routes.ts
@@ -2,10 +2,13 @@ import type { Request, Response } from "express";
 
 import { Router } from "express";
 
+import { envConfig } from "@/config";
+import { authLoginSchema } from "@/domain/auth/schemas";
 import { createUserSchema } from "@/domain/user/schemas";
 import { expressAdapter } from "@/presentation/adapters/express";
-import { authRegisterController } from "@/presentation/service-provider";
+import { authLoginController, authRegisterController } from "@/presentation/service-provider";
 
+import { accessTokenCookieConfig, refreshTokenCookieConfig } from "../config/cookies";
 import { validateRequest } from "../middlewares";
 
 /**
@@ -20,6 +23,21 @@ authRoutes.post("/register", validateRequest({ body: createUserSchema }), async 
   const res = await expressAdapter(request, authRegisterController);
 
   response.status(res.statusCode).json(res.body);
+});
+
+/**
+ * Endpoint to log in a user.
+ */
+authRoutes.post("/login", validateRequest({ body: authLoginSchema }), async (request: Request, response: Response) => {
+  const res = await expressAdapter(request, authLoginController);
+
+  const { accessToken, refreshToken } = res.body.data;
+
+  // Set the access and refresh token cookies
+  response.cookie(envConfig.auth.access_token_name, accessToken, accessTokenCookieConfig);
+  response.cookie(envConfig.auth.refresh_token_name, refreshToken, refreshTokenCookieConfig);
+
+  response.status(res.statusCode).json({ message: res.body.message, accessToken });
 });
 
 export { authRoutes };
