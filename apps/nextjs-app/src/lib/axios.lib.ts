04eeb454cfadd76bc0f7c59fffb486392a9da3a9
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2025-05-28 14:47:41 +0100

    refactor: reorganize imports and ensure consistent formatting across components

--- apps/nextjs-app/src/lib/axios.lib.ts
+++ apps/nextjs-app/src/lib/axios.lib.ts
@@ -1,5 +1,6 @@
-import { API_URL } from "@/constants/env.constants";
 import axios from "axios";
+
+import { API_URL } from "@/constants/env.constants";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
